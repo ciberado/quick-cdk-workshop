19b8c630f072d4514c80f010d901de5625127090
Javi Moreno <email@javier-moreno.com>
2022-01-14 15:25:01 +0100

    Kept simple version

--- asg/index.ts
+++ asg/index.ts
@@ -5,99 +5,37 @@ import ec2 = require('@aws-cdk/aws-ec2');
 import autoscaling = require('@aws-cdk/aws-autoscaling');
 import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
 
-class PetclinicVPCStack extends cdk.Stack {
-  readonly vpc : ec2.Vpc;
-  readonly privateSubnets: ec2.SelectedSubnets;
-
-  readonly dbSecurityGroup : ec2.SecurityGroup;
-  readonly asgSecurityGroup: ec2.SecurityGroup;
-  readonly lbSecurityGroup : ec2.SecurityGroup;
-
+class PokemonStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    this.vpc = new ec2.Vpc(this, 'VPC' + process.env.USER);
+    const vpc = new ec2.Vpc(this, 'VPC' + process.env.USER);
 
-    this.privateSubnets = this.vpc.selectSubnets({subnetType: ec2.SubnetType.PRIVATE_WITH_NAT});
-
-    this.dbSecurityGroup = new ec2.SecurityGroup(this, 'SG-DB' + process.env.USER, {
-      vpc: this.vpc
-    });
-    this.asgSecurityGroup = new ec2.SecurityGroup(this, 'SG-APP' + process.env.USER, {
-      vpc: this.vpc
-    });
-    this.lbSecurityGroup = new ec2.SecurityGroup(this, 'SG-ALB' + process.env.USER, {
-      vpc: this.vpc
-    });
-    this.dbSecurityGroup.addIngressRule(this.asgSecurityGroup, ec2.Port.tcp(3306), 'Mysql default port for App layer');
-    this.asgSecurityGroup.addIngressRule(this.lbSecurityGroup, ec2.Port.tcp(8080), 'App port for Load Balancer');
-    this.lbSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Http open to anybody');
-  }
-}
-
-class PetclinicDBStack extends cdk.Stack {
-  readonly mysql: rds.DatabaseInstance;
-  
-  constructor(scope: cdk.App, id: string, 
-              vpc: ec2.Vpc, privateSubnets: ec2.SelectedSubnets, 
-              dbSecurityGroup : ec2.SecurityGroup,
-              props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    this.mysql = new rds.DatabaseInstance(this, 'DBMain' + process.env.USER, {
-      engine : rds.DatabaseInstanceEngine.mysql({
-        version: rds.MysqlEngineVersion.VER_5_7
-      }),       
-      deletionProtection : false,
-      multiAz : false,
-      vpc : vpc,
-      instanceIdentifier : 'pokemonDB' + process.env.USER,
-      vpcSubnets : privateSubnets,
-      securityGroups: [dbSecurityGroup],
-      credentials : {
-        username : 'admin',
-        password : cdk.SecretValue.plainText('p3tcl1n1c')
-      }
-    });
-  }
-}
-
-class PetclinicAppStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, 
-    vpc: ec2.Vpc, privateSubnets: ec2.SelectedSubnets, 
-    lbSecurityGroup: ec2.SecurityGroup,
-    asgSecurityGroup : ec2.SecurityGroup,
-    mysql: rds.DatabaseInstance,
-    props?: cdk.StackProps) {
-    super(scope, id, props);
+    const privateSubnets = vpc.selectSubnets({subnetType: ec2.SubnetType.PRIVATE_WITH_NAT});
 
     const userData = ec2.UserData.custom(
 `#!/bin/sh
 
 sudo apt update
-sudo apt install openjdk-8-jdk-headless -y
+sudo apt install openjdk-8-jre-headless -y
 
 # pokemon
-git clone https://github.com/spring-petclinic/spring-framework-petclinic.git
-cd spring-framework-petclinic
-
-RDS=${mysql.dbInstanceEndpointAddress}
-./mvnw jetty:run-war -P MySQL -DskipTests
+wget https://github.com/ciberado/pokemon/releases/download/stress/pokemon-0.0.4-SNAPSHOT.jar
+java -jar pokemon-0.0.4-SNAPSHOT.jar
 
 `);
     const ami = ec2.MachineImage.genericLinux({
       'eu-west-1': 'ami-020fc399c31009b50',
       'eu-central-1': 'ami-0215371f3ea49a91b'
     });
-    const asg = new autoscaling.AutoScalingGroup(this, 'ASG' + process.env.USER, {
+    const asg = new autoscaling.AutoScalingGroup(this, 'pokemonASG' + process.env.USER, {
       vpc,
-      securityGroup : asgSecurityGroup,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       machineImage: ami,
       userData: userData,
       vpcSubnets: privateSubnets,
       maxCapacity: 4,
-      minCapacity: 1     
+      minCapacity: 1      
     });
     asg.role.addManagedPolicy(
       iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
@@ -108,10 +46,25 @@ RDS=${mysql.dbInstanceEndpointAddress}
       estimatedInstanceWarmup : cdk.Duration.seconds(60)
     });
 
-    const lb = new elbv2.ApplicationLoadBalancer(this, 'ALB' + process.env.USER, {
+    const postgres = new rds.DatabaseInstance(this, 'pokemonDBMain' + process.env.USER, {
+      engine : rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_12_4,
+      }),       
+      deletionProtection : false,
+      multiAz : false,
+      vpc : vpc,
+      instanceIdentifier : 'pokemonDB' + process.env.USER,
+      vpcSubnets : privateSubnets,
+      credentials : {
+        username : 'postadmin',
+        password : cdk.SecretValue.plainText('supersecret')
+      }
+    });
+    postgres.connections.allowDefaultPortFrom(asg);
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'pokemonALB' + process.env.USER, {
       vpc,
-      internetFacing: true,
-      securityGroup: lbSecurityGroup
+      internetFacing: true
     });
     const listener = lb.addListener('Listener' + process.env.USER, {
       port: 80,
@@ -129,30 +82,17 @@ RDS=${mysql.dbInstanceEndpointAddress}
     new cdk.CfnOutput(this, 'LoadBalancerDNS', {
       value: lb.loadBalancerDnsName
     });
+
+
   }
 }
 
+
 const app = new cdk.App();
 
-const globalTags = {
-  'project' : 'quick-cdk-workshop',
-  'owner' : process.env.USER!
-};
-
-const vpcStack = new PetclinicVPCStack(app, 'petclinic-VPC-' + process.env.USER, {
-  tags: globalTags
+new PokemonStack(app, 'pokemon-' + process.env.USER, {
+  tags : {
+    'project' : 'quick-cdk-workshop',
+    'owner' : process.env.USER!
+  }
 });
-
-const dbStack = new PetclinicDBStack(app, 'petclinic-DB-' + process.env.USER, 
-  vpcStack.vpc, vpcStack.privateSubnets, vpcStack.dbSecurityGroup, {
-  tags: globalTags
-});
-dbStack.addDependency(vpcStack);
-
-const appStack = new PetclinicAppStack(app, 'petclinic-APP-' + process.env.USER, 
-  vpcStack.vpc, vpcStack.privateSubnets, 
-  vpcStack.lbSecurityGroup, vpcStack.asgSecurityGroup, dbStack.mysql, {
-  tags: globalTags
-});
-
-appStack.addDependency(dbStack);
